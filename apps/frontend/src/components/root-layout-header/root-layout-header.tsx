71ea2d5bbde97078f3bb586688900937e04b1c9d
Mahziyar Esmaili <mahziyar.esmaili.dev@gmail.com>
2024-08-16 07:06:13 +0200

    header should only have a bottom border in dark mode

--- apps/frontend/src/components/root-layout-header/root-layout-header.tsx
+++ apps/frontend/src/components/root-layout-header/root-layout-header.tsx
@@ -2,7 +2,7 @@ import { ThemeSwitcher } from '@/components/theme-switcher';
 
 export const RootLayoutHeader = () => {
   return (
-    <div className="h-[50px] p-4 flex items-center justify-between bg-primary dark:bg-background ">
+    <div className="h-[50px] p-4 flex items-center justify-between bg-primary dark:bg-background dark:border-b border-border">
       <div className="text-primary-foreground dark:text-foreground">
         File Manager
       </div>
