a53f6e26a9a22d2fdfba94d67d5f78954e136a4f
Mahziyar Esmaili <mahziyar.esmaili.dev@gmail.com>
2024-08-17 10:22:52 +0200

    imported Folder repository in folders module

--- apps/backend/src/folders/folders.module.ts
+++ apps/backend/src/folders/folders.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FoldersController } from './folders.controller';
 import { FoldersService } from './folders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Folder } from './folder.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Folder])],
   controllers: [FoldersController],
   providers: [FoldersService],
   exports: [FoldersService],
