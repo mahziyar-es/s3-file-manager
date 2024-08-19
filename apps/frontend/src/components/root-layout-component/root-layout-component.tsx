989b4cb2cc356d2c5e5242d4942a5b104cb9cce7
Mahziyar Esmaili <mahziyar.esmaili.dev@gmail.com>
2024-08-19 07:45:02 +0200

    installed shadcn sonner component

--- apps/frontend/src/components/root-layout-component/root-layout-component.tsx
+++ apps/frontend/src/components/root-layout-component/root-layout-component.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { ReactNode } from 'react';
 import { RootLayoutHeader } from '@/components/root-layout-header';
+import { Toaster } from '@/components/ui/sonner';
 
 export const RootLayoutComponent = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col h-full w-full">
       <RootLayoutHeader />
       <div className="flex-1 overflow-hidden">{children}</div>
+      <Toaster richColors />
     </div>
   );
 };
